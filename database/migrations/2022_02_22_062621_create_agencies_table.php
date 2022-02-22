30c818456f014f54365a5e7e62a9bd3829181936
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-02-22 15:20:43 +0530

    Added User model and Agency model with migrations

--- database/migrations/2022_02_22_062621_create_agencies_table.php
+++ database/migrations/2022_02_22_062621_create_agencies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAgenciesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('agencies', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('usertype');
-            $table->timestamp('status');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('address');
+            $table->string('reg_no');
+            $table->string('license_no');
+            $table->date('license_exp_date');
+            $table->string('website');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('agencies');
     }
 }
