ad7b1bd5f9eb41ea80e314873b95ba85b496d6a7
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-03-21 09:21:25 +0530

    added Query model and migrations

---
+++ database/migrations/2022_03_21_034540_create_queries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQueriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queries', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade');
+
+            $table->string('to');
+            $table->string('description');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('queries');
+    }
+}
